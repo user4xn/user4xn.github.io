fbafdf79598a3f35622fcda7b1e95f5a38236c7a
Wildan Kurnia Candra <37010270+user4xn@users.noreply.github.com>
2024-02-12 08:41:46 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Add your configurations here
+    experimental: {
+      // Add experimental features here, if needed
+      images: true // Enable experimental images feature
+    },
+    // Add other configurations as needed
+    // For example, if you're planning to use static HTML export:
+    output: {
+      // Set up static HTML export
+      export: true
+    }
+  };
+  
+  module.exports = nextConfig;
+  
